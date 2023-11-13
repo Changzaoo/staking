547f9e4257faaf08fa05173e439e94c255262cd8
Vinicius <redcanidsvinicius@gmail.com>
2023-11-13 11:19:09 -0300

    Merge changes from remote repository

--- consts/contractAddresses.ts
+++
@@ -1,6 +0,0 @@
-export const nftDropContractAddress =
-  "0x09F054c1F8BF1a57f46978baA89d593A118221B5";
-export const tokenContractAddress =
-  "0xfb7Dd4C9e168b3e3cfCbf6890d9D729e30Fb2F6C";
-export const stakingContractAddress =
-  "0x9A6426482c735817F00eA363A8F015405B9D1242";
